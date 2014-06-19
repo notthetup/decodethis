143b29a90958add626225e54142b3ba9d3a743d0
Chinmay Pendharkar <notthetup@gmail.com>
2014-06-19 18:09:58 +0800

    added basic structure

---
+++ src/app.js
@@ -0,0 +1,4 @@
+// get files.json.
+// for each file create dom elemement
+// for each file try to load and update dom
+// calculate score
---
+++ src/loadanddecode.js
@@ -0,0 +1,38 @@
+/*
+* Downloads a sound file using XHR and decodes it using WebAudio
+*
+* @method loadanddecode
+* @param {String} URL URL of the audio file to be downloaded.
+* @param {Function} onLoadCallback Callback for when the decoded AudioBuffer is ready. Callback returns an {Error} (if any) and an {AudioBuffer}
+* @param {Function} onProgressCallback Callback for progress event from the XHR download.
+* @param {AudioContext} [AudioContext] Optional AudioContext to be used for decoding.
+*
+*/
+function loadanddecode(URL, onLoadCallback, onProgressCallback, audioContext){
+    if (!audioContext){
+        window.AudioContext = window.AudioContext || window.webkitAudioContext
+        audioContext = new AudioContext();
+    }
+    var request = new XMLHttpRequest();
+    request.open('GET', URL, true);
+    request.responseType = 'arraybuffer';
+    request.onload = function () {
+        audioContext.decodeAudioData(request.response, function(buffer){
+            if (typeof onLoadCallback === 'function')
+                onLoadCallback(null, buffer);
+        },function (){
+            if (typeof onLoadCallback === 'function')
+                onLoadCallback(new Error("Decoding Error"), null);
+        });
+    };
+    request.onerror = function(){
+        if (typeof onLoadCallback === 'function')
+            onLoadCallback(new Error("Loading Error"), null);
+    }
+    request.onprogress = function(event){
+        if (typeof onProgressCallback === 'function'){
+            onProgressCallback(event);
+        }
+    }
+    request.send();
+}
