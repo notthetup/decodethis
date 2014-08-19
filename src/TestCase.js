62bc9558f8f42fcb79249f5c533de51d17866aa8
Chinmay Pendharkar <notthetup@gmail.com>
2014-08-19 21:19:50 +0800

    changed to TestCase

---
+++ src/TestCase.js
@@ -0,0 +1,45 @@
+function TestCase (testFileJSON, audioContext){
+
+	if (!testFileJSON)
+		return;
+
+	testFileJSON.status = "Untested";
+
+
+	this.data = testFileJSON;
+	this.data.class = "untested";
+	this.data.action = "▶";
+	this.onFinish = null;
+	this.onProgress = null;
+	this.onReset = null;
+	this.percentComplete = 0;
+
+	this.start = function(){
+		self = this;
+		this.status = "running";
+		loadanddecode(this.data.path + this.data.filename, function (error, buffer){
+			self.status = "complete";
+			if (typeof self.onFinish === 'function'){
+				self.onFinish(error, buffer);
+			}
+		}, function (event){
+			if (event.lengthComputable) {
+				self.percentComplete = Math.round(100*event.loaded/event.total);
+			}
+			if (typeof self.onProgress === 'function'){
+				self.onProgress(self.percentComplete);
+			}
+		}, audioContext);
+	};
+
+	this.reset = function(){
+		this.status = "init";
+		this.percentComplete = 0;
+		if (typeof this.onReset === 'function'){
+			this.onReset();
+		}
+	};
+
+	this.status = "init";
+}
+
